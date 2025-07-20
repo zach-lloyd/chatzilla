2c1f8685c24b9d588f7b33376af3fea420c710b0
Zach Lloyd <joseph.z.lloyd@gmail.com>
2025-07-20 15:33:45 -0400

    Remove unnecessary Modal component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,9 @@
 import ChatzillaLogo from "../assets/ChatzillaLogo.png";
 
 function Logo() {
-    return (
-        <img 
-            src={ChatzillaLogo} 
-            alt="Image of a cartoon monster" 
-            className="w-50 h-auto"
-        />
-    )
+  return (
+    <img src={ChatzillaLogo} alt="Cartoon monster" className="w-50 h-auto" />
+  );
 }
 
 export default Logo;
