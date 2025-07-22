cf9cedcc85aa6a21a1eb17439db81d4e57ff0eb8
Zach Lloyd <joseph.z.lloyd@gmail.com>
2025-07-22 16:54:24 -0400

    clean up user and room controllers

--- messenger_api/app/controllers/rooms_controller.rb
+++ messenger_api/app/controllers/rooms_controller.rb
@@ -12,14 +12,16 @@ class RoomsController < ApplicationController
   end
 
   def create
-    # Build the room and assign the creator (current_user)
+    # Build the room and assign the creator (current_user).
     @room = current_user.created_rooms.build(room_params)
 
     if @room.save
-      # --- Automatically add the creator as a member ---
+      # Automatically add the creator as a member.
       @room.users << current_user unless @room.users.include?(current_user)
 
-      render json: @room, include: { users: { only: [:id, :username] } }, status: :created
+      render json: @room, 
+             include: { users: { only: [:id, :username] } }, 
+             status: :created
     else
       render json: @room.errors, status: :unprocessable_entity
     end
@@ -28,36 +30,25 @@ class RoomsController < ApplicationController
   # GET /rooms/:id
   def show
     begin
-      # Use includes to eager-load associated users (members) to avoid N+1 queries
+      # Use includes to eager-load associated users (members) to avoid N + 1 
+      # queries.
       @room = Room.includes(:users, messages: :user).find(params[:id])
 
-      # --- Authorization Check ---
-      # Allow if room is public OR if user is a member
-      #unless @room.public? || @room.users.include?(current_user)
-       # render json: { error: 'You do not have permission to view this room.' }, status: :forbidden
-        #return # Stop execution
-      #end
-      # ---
-
       render json: @room, include: {
-        users: { # Members of the room
+        users: {
           only: [:id, :username]
         },
-        messages: { # Messages in the room
-          # You might want to order messages, e.g., by creation date
-          # This requires adjusting the association or adding an order scope
+        messages: { 
           include: {
-            user: { # Author of the message
+            user: { 
               only: [:id, :username]
             }
           }
-          # Optionally limit message fields:
-          # only: [:id, :body, :created_at, :user_id] # user_id might be useful too
         }
       }
 
     rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Room not found' }, status: :not_found
+      render json: { error: "Room not found" }, status: :not_found
     end
   end
 
--- messenger_api/app/controllers/users/sessions_controller.rb
+++ messenger_api/app/controllers/users/sessions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
   before_action :set_cache_buster
 
   include ActionController::Flash
@@ -22,13 +21,6 @@ class Users::SessionsController < Devise::SessionsController
   def destroy
     super
   end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 
   private
 
