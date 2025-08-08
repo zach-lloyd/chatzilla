f539725824487a7f602ceda67df206e21c96fb88
Zach Lloyd <joseph.z.lloyd@gmail.com>
2025-08-08 16:10:35 -0400

    configure session store for production

--- messenger_api/config/initializers/cors.rb
+++ messenger_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://chatzilla-front-end.onrender.com"
+    origins "http://localhost:3000", "http://localhost:5173", "https://chatzilla-front-end.onrender.com"
 
     resource "*",
       headers: :any,
--- messenger_api/config/initializers/session_store.rb
+++ messenger_api/config/initializers/session_store.rb
@@ -1,4 +1,12 @@
-Rails.application.config.session_store :cookie_store,
-                                       key: '_odin_messenger_session', # Replace with your app's name
-                                       same_site: :lax,
-                                       secure: Rails.env.production?
+if Rails.env.production?
+  # Configuration for the production environment on Render.
+  Rails.application.config.session_store :cookie_store,
+    key: "_chatzilla_session", 
+    domain: :all,             
+    same_site: :none,         
+    secure: true,             
+    tld_length: 2             
+else
+  # Default configuration for the development environment.
+  Rails.application.config.session_store :cookie_store, key: "_chatzilla_session_dev"
+end
