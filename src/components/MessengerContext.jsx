4528b50a29609a76482c112d2d8fe363e5ac8156
Zach Lloyd <joseph.z.lloyd@gmail.com>
2025-08-08 15:22:28 -0400

    remove hardcoded backend URL and replace with environment variable

--- src/components/MessengerContext.jsx
+++ src/components/MessengerContext.jsx
@@ -51,8 +51,9 @@ export function processErrors(errorData) {
 export const MessengerContext = createContext();
 
 export const MessengerProvider = ({ children }) => {
-  const BASE_URL =
-    "https://chatzilla-1ntu.onrender.com" || "http://localhost:3000";
+  const BASE_URL = import.meta.env.PROD
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:3000";
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
   const [csrfToken, setCsrfToken] = useState(null);
